2cd2908feb960a02ddc0ecd83c3018b4893dce56
lab2-26 <lab2-26@LAB2-26>
2025-10-28 18:53:38 -0600

    Agregar archivos de proyecto.

---
+++ MiPrimeraAPI/Controllers/ProductosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiPrimeraAPI.Models;
+
+namespace MiPrimeraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private static readonly List<Models.Producto> productos = new()
+        {
+            new Models.Producto { Id = 1, Nombre = "Producto A", Precio = 10.99m, Stock = 100 },
+            new Models.Producto { Id = 2, Nombre = "Producto B", Precio = 15.49m, Stock = 50 },
+            new Models.Producto { Id = 3, Nombre = "Producto C", Precio = 7.99m, Stock = 200 }
+        };
+        [HttpGet]
+        public ActionResult<List<Producto>> GetProductos()
+        {
+            return Ok(productos);
+        }
+    }
+}
---
+++ MiPrimeraAPI/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace MiPrimeraAPI.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public  required string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
