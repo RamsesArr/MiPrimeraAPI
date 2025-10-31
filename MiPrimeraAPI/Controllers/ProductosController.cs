e0d1c2ab6321c13aee3e017a0142e2135bc445f9
lab2-26 <lab2-26@LAB2-26>
2025-10-30 18:47:55 -0600

    Completo

--- MiPrimeraAPI/Controllers/ProductosController.cs
+++ MiPrimeraAPI/Controllers/ProductosController.cs
@@ -19,5 +19,49 @@ namespace MiPrimeraAPI.Controllers
         {
             return Ok(productos);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Producto> GetProducto(int id)
+        {
+            var producto = productos.FirstOrDefault(p => p.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return Ok(producto);
+        }
+        [HttpPost]
+        public ActionResult<Producto> CrearProducto([FromBody] Producto nuevoProducto)
+        {
+            nuevoProducto.Id = productos.Max(p => p.Id) + 1;
+            productos.Add(nuevoProducto);
+            return CreatedAtAction(nameof(GetProducto), new { id = nuevoProducto.Id }, nuevoProducto);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult ActualizarProducto(int id, [FromBody] Producto productoActualizado)
+        {
+            var producto = productos.FirstOrDefault(p => p.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            producto.Nombre = productoActualizado.Nombre;
+            producto.Precio = productoActualizado.Precio;
+            producto.Stock = productoActualizado.Stock;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult EliminarProducto(int id)
+        {
+            var producto = productos.FirstOrDefault(p => p.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            productos.Remove(producto);
+            return NoContent();
+        }
     }
 }
